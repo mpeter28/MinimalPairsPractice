ddd19476d9a037ea6a3074f9be1bdf74ab9c08b6
mpeter28 <marciepeters28@yahoo.com>
2019-09-25 13:07:37 -0700

    Got react integrated

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,19 @@ module.exports = {
             loader: 'less-loader',
           },
         ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
       }
     ]
   }
