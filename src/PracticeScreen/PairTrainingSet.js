6d7fe344811f5c8e8ee2c39fcad82cc728c7dd6b
mpeter28 <marciepeters28@yahoo.com>
2019-09-26 10:23:51 -0700

    Except for the audio issue, it is otherwise usable

--- src/PracticeScreen/PairTrainingSet.js
+++ src/PracticeScreen/PairTrainingSet.js
@@ -19,27 +19,102 @@ export default class PairTrainingSet extends Component {
         successCount: 0,
         failureCount: 0,
         waitingToStart: true,
-        useFirstOption: Math.random > 0.5,
+        lastGuessSuccess: true,
+        useFirstOption: Math.random() > 0.5,
     };
 
     render() {
         return <div className="pairTrainingSet">
             <h3 className="pairTrainingSet-name">{ this.props.chosenTrainingPair.name }</h3>
 
-            <audio controls className="pairTrainingSet-audioPlayer">
-                <source src={this.getTestAudioUrl()} type="audio/ogg" />
-                Your browser does not support the audio element.
-            </audio>
+            { this.renderMainContent() }
+        </div>;
+    }
 
-            <div className="pairTrainingSet-controls">
-                <button className="pairTrainingSet-optionOne">
-                    {this.props.chosenTrainingPair.optionOneName}
-                </button>
+    renderMainContent() {
 
-                <button className="pairTrainingSet-optionTwo">
-                    {this.props.chosenTrainingPair.optionTwoName}
-                </button>
-            </div>
+        if (this.state.waitingToStart) {
+            if (this.state.successCount === 0 && this.state.failureCount === 0) { // Start screen
+                return <div>
+                    { this.getAudioPlayer() }
+                    { this.getStartButton() }
+                </div>;
+
+            } else if (this.state.successCount === 0 + this.state.failureCount === 20) { // End screen
+                return <div>
+                    { this.getAudioPlayer() }
+                    { this.getScoreCard() }
+                    { this.getDoneButton() }
+                </div>;
+
+            } else { // results screen
+                return <div>
+                    { this.getAudioPlayer() }
+                    { this.getResultsFromLastRound() }
+                    { this.getScoreCard() }
+                    { this.getStartButton() }
+                </div>;
+
+            }
+        } else { // guessing screen
+            return <div>
+                { this.getAudioPlayer() }
+                { this.getGuessControls() }
+            </div>;
+        }
+    }
+
+    getDoneButton() {
+        return <button className="pairTrainingSet-doneButton" onClick={this.done()}>
+            Choose Next Pair
+        </button>;
+    }
+
+    done() {
+        return () => {
+            (this.state.successCount >= 16
+                ? this.props.trainingPassedHandler
+                : this.props.trainingFailedHandler)();
+        };
+    }
+
+    getResultsFromLastRound() {
+        return <p className="pairTrainingSet-roundResults">
+            { this.state.lastGuessSuccess ? 'Correct!' : 'Wrong :(' }
+        </p>;
+    }
+
+    getStartButton() {
+        return <button className="pairTrainingSet-startButton" onClick={() => this.setState({ waitingToStart: false })}>
+            Begin
+        </button>;
+    }
+
+    getScoreCard() {
+        return <div className="pairTrainingSet-scoreCard">
+            <span>{this.state.successCount} Correct</span>
+            <span>{this.state.failureCount} Failed</span>
+        </div>
+    }
+
+    getAudioPlayer() {
+        return <audio controls autoPlay={true} className="pairTrainingSet-audioPlayer">
+            <source src={this.getTestAudioUrl()} type="audio/ogg" />
+            Your browser does not support the audio element.
+        </audio>;
+    }
+
+    getGuessControls() {
+        return <div className="pairTrainingSet-controls">
+            <button className="pairTrainingSet-optionOne"
+                    onClick={this.guessOptionOne()}>
+                {this.props.chosenTrainingPair.optionOneName}
+            </button>
+
+            <button className="pairTrainingSet-optionTwo"
+                    onClick={this.guessOptionTwo()}>
+                {this.props.chosenTrainingPair.optionTwoName}
+            </button>
         </div>;
     }
 
@@ -50,10 +125,30 @@ export default class PairTrainingSet extends Component {
     }
 
     guessOptionOne() {
-        
+        return () => {
+            this.setState((state) => {
+                return {
+                    successCount: state.successCount + (state.useFirstOption ? 1 : 0),
+                    failureCount: state.failureCount + (state.useFirstOption ? 0 : 1),
+                    waitingToStart: true,
+                    lastGuessSuccess: state.useFirstOption,
+                    useFirstOption: Math.random() > 0.5,
+                }
+            });
+        }
     }
 
     guessOptionTwo() {
-
+        return () => {
+            this.setState((state) => {
+                return {
+                    successCount: state.successCount + (state.useFirstOption ? 0 : 1),
+                    failureCount: state.failureCount + (state.useFirstOption ? 1 : 0),
+                    waitingToStart: true,
+                    lastGuessSuccess: !state.useFirstOption,
+                    useFirstOption: Math.random() > 0.5,
+                }
+            });
+        }
     }
 }
